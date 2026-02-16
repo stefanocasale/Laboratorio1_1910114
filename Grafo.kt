6f3f6dfaef2b66778d5c53f921047e883aa92d8c
stefanocasale <stefanocasale000@gmail.com>
2026-02-16 19:19:48 -0400

    Interfaz del Grafo

---
+++ Grafo.kt
@@ -0,0 +1,11 @@
+// Interfaz
+interface Grafo<T> {
+    fun agregarVertice(v: T): Boolean
+    fun eliminarVertice(v: T): Boolean
+    fun conectar(desde: T, hasta: T): Boolean
+    fun contiene(v: T): Boolean
+    fun obtenerArcosSalida(v: T): List<T>
+    fun obtenerArcosEntrada(v: T): List<T>
+    fun tamano(): Int
+    fun subgrafo(vertices: Collection<T>): Grafo<T>
+}
